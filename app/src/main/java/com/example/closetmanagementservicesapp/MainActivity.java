98e4ba31b1073fadc132217d35aa6228aa1bbdbe
juyoungck <juyoungck@gmail.com>
2024-07-22 17:47:15 +0900

    DB 파일 생성 및 테이블 생성

--- app/src/main/java/com/example/closetmanagementservicesapp/MainActivity.java
+++ app/src/main/java/com/example/closetmanagementservicesapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.closetmanagementservicesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // MyApplication 클래스에서 DBHelper를 가져오는 코드
+        MyApplication app = (MyApplication) getApplicationContext();
+        dbHelper = app.getDbHelper();
+
+        // db 파일을 읽기/쓰기가 가능한 형식으로 연다
+        db = dbHelper.getWritableDatabase();
+
+        // 추후 db 작업 코드 추가 예정 (데이터 insert, delete, ... 등)
+
+    }
+
+
+    // MainActivity가 종료 될 때 호출 되는 메서드
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // MyApplication 클래스에서 앱이 정상적으로 종료될 때 db 파일을 닫는 코드가 있지만
+        // 정상적으로 종료되지 않았을 때는 db 파일이 닫히지 앋기 때문에 이 곳에도 코드를 추가함
+        if (db != null && db.isOpen()) {
+            db.close();
+        }
     }
 }
---
+++ app/src/main/java/com/example/closetmanagementservicesapp/MyApplication.java
@@ -0,0 +1,29 @@
+package com.example.closetmanagementservicesapp;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    private DBHelper dbHelper;
+
+    public void onCreate() {
+        super.onCreate();
+
+        // DBHelper 생성
+        dbHelper = new DBHelper(this);
+    }
+
+    public DBHelper getDbHelper() {
+        return dbHelper;
+    }
+
+    // 앱이 사용자에 의해 정상적으로 종료될 때 호출 되는 메서드
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        // 앱 종료 시 db 파일 닫음
+        if (dbHelper != null) {
+            dbHelper.close();
+        }
+    }
+}
