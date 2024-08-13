867e8a84c394e88437be021e94a6142656f0f9b8
LimKyungSup <165024970+LimKyungSup@users.noreply.github.com>
2024-08-13 22:46:43 +0900

    수정 버튼 기능 추가

--- app/src/main/java/com/example/closetmanagementservicesapp/MainActivity.java
+++ app/src/main/java/com/example/closetmanagementservicesapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -11,12 +12,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Button;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.CheckBox;
@@ -52,6 +58,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 코디 탭 이동
+        Button btnCody = (Button) findViewById(R.id.btnCody);
+        btnCody.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Cody.class);
+                startActivity(intent);
+            }
+        });
+
         // 설정 탭 이동
         ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings);
         btnSettings.setOnClickListener(new View.OnClickListener() {
@@ -66,13 +81,13 @@ public class MainActivity extends AppCompatActivity {
         btnSort.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
-                View view1 = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_sort, null);
+                View tabView = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_sort, null);
 
-                bottomSheetDialog.setContentView(view1);
+                bottomSheetDialog.setContentView(tabView);
                 bottomSheetDialog.show();
 
                 // 정렬 버튼 닫기
-                ImageButton tabsortClose = view1.findViewById(R.id.tabsortClose);
+                ImageButton tabsortClose = tabView.findViewById(R.id.tabsortClose);
                 tabsortClose.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -84,10 +99,39 @@ public class MainActivity extends AppCompatActivity {
                 TabSort tabsort = new TabSort();
 
                 // 정렬 (옷 종류)
-                tabsort.clothesSelect(view1);
+                tabsort.clothesSelect(tabView);
 
                 // 정렬 (날씨)
-                tabsort.weatherSelect(view1);
+                tabsort.weatherSelect(tabView);
+            }
+        });
+
+        // 수정 버튼
+        ImageButton btnModify = (ImageButton) findViewById(R.id.btnModify);
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                View modifyView = LayoutInflater.from(MainActivity.this).inflate(R.layout.tab_modify, null);
+
+                dialog.setContentView(modifyView);
+                dialog.show();
+
+                WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width=WindowManager.LayoutParams.WRAP_CONTENT;
+                params.height=1200;
+                dialog.getWindow().setAttributes(params);
+
+                ImageButton tabmodifyClose = modifyView.findViewById(R.id.tabmodifyClose);
+                tabmodifyClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                TabModify tabModify = new TabModify();
+
+                tabModify.modifyButton(modifyView);
             }
         });
 
