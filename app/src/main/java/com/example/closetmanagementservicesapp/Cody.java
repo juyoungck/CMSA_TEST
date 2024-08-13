3214caf7138d87527d6fe4dba6296bb1408c80d1
LimKyungSup <165024970+LimKyungSup@users.noreply.github.com>
2024-08-13 22:45:46 +0900

    Create Cody.java

---
+++ app/src/main/java/com/example/closetmanagementservicesapp/Cody.java
@@ -0,0 +1,16 @@
+package com.example.closetmanagementservicesapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Cody extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cody);
+
+
+    }
+}
