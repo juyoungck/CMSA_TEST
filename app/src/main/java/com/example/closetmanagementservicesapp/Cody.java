92454d2afb4e0ea6cf7cdcc4b569883c409bb2c2
LimKyungSup <165024970+LimKyungSup@users.noreply.github.com>
2024-08-19 22:02:19 +0900

    코디 화면 기능 추가

--- app/src/main/java/com/example/closetmanagementservicesapp/Cody.java
+++ app/src/main/java/com/example/closetmanagementservicesapp/Cody.java
@@ -1,16 +1,135 @@
 package com.example.closetmanagementservicesapp;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Button;
+import androidx.appcompat.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.view.View;
+
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Cody extends AppCompatActivity {
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cody);
+        setContentView(R.layout.activity_main);
 
+        // 하단 등록 버튼 이동
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddMenu.class);
+                startActivity(intent);
+            }
+        });
 
+        // 코디 탭 이동
+        Button btnCloset = (Button) findViewById(R.id.btnCloset);
+        btnCloset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 설정 탭 이동
+        ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        // 정렬 버튼 창
+        ImageButton btnSort = (ImageButton) findViewById(R.id.btnSort);
+        btnSort.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(Cody.this);
+                View tabView = LayoutInflater.from(Cody.this).inflate(R.layout.tab_sort, null);
+
+                bottomSheetDialog.setContentView(tabView);
+                bottomSheetDialog.show();
+
+                // 정렬 버튼 닫기
+                ImageButton tabsortClose = tabView.findViewById(R.id.tabsortClose);
+                tabsortClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                // 정렬 기능 호출 (TabSort 클래스)
+                TabSort tabsort = new TabSort();
+
+                // 정렬 (옷 종류)
+                tabsort.clothesSelect(tabView);
+
+                // 정렬 (날씨)
+                tabsort.weatherSelect(tabView);
+            }
+        });
+
+        // 수정 버튼
+        ImageButton btnModify = (ImageButton) findViewById(R.id.btnModify);
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(Cody.this);
+                View modifyView = LayoutInflater.from(Cody.this).inflate(R.layout.tab_modify, null);
+
+                dialog.setContentView(modifyView);
+                dialog.show();
+
+                WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+                params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                params.height = 1200;
+                dialog.getWindow().setAttributes(params);
+
+                ImageButton tabmodifyClose = modifyView.findViewById(R.id.tabmodifyClose);
+                tabmodifyClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                TabModify tabModify = new TabModify();
+
+                tabModify.modifyButton(modifyView);
+            }
+        });
     }
 }
