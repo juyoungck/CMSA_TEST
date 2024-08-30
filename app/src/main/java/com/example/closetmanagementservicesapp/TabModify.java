ede6205a460993475ead8947b73cd4c19263c5c2
LimKyungSup <165024970+LimKyungSup@users.noreply.github.com>
2024-08-30 18:55:44 +0900

    수정 버튼 기능 추가 (리스트뷰 / 추가, 수정, 삭제)

--- app/src/main/java/com/example/closetmanagementservicesapp/TabModify.java
+++ app/src/main/java/com/example/closetmanagementservicesapp/TabModify.java
@@ -1,28 +1,48 @@
 package com.example.closetmanagementservicesapp;
 
+
+import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TabModify {
+
+    ListView listView;
+    ArrayList<String> itemData;
+    TabModifyAdapter tabModifyAdapter;
+
 
     public void modifyButton(View modifyView) {
         Button modify_Add = modifyView.findViewById(R.id.modify_Add);
-        Button modify_Modify = modifyView.findViewById(R.id.modify_Modify);
-        Button modify_delete = modifyView.findViewById(R.id.modify_delete);
-        ImageButton modify_closet_edit = modifyView.findViewById(R.id.modify_closet_edit);
-        TextView closet1 = modifyView.findViewById(R.id.closet1);
-        EditText modify_closet_editText = modifyView.findViewById(R.id.modify_closet_editText);
-        ImageButton modify_closet_delete = modifyView.findViewById(R.id.modify_closet_delete);
+        CheckBox modify_Modify = modifyView.findViewById(R.id.modify_Modify);
 
-        modify_closet_edit.setVisibility(View.INVISIBLE);
-        modify_closet_delete.setVisibility(View.INVISIBLE);
-        modify_closet_editText.setVisibility(View.INVISIBLE);
+        // 리스트뷰 참조
+        listView = modifyView.findViewById(R.id.list_item);
+        itemData = new ArrayList<String>();
+
+        // 어댑터 클래스 참고
+        tabModifyAdapter = new TabModifyAdapter(modifyView.getContext(), itemData, new TabModifyAdapter.DeleteItem(){
+            @Override
+            public void Delete(int position) {
+                itemData.remove(position);
+                tabModifyAdapter.notifyDataSetChanged();
+            }
+        });
+
 
         /* 추가 버튼 클릭시 다이얼로그
         충돌 발생해서 protected void --> public void 으로 변경했습니다.*/
@@ -30,6 +50,7 @@ public class TabModify {
         modify_Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(modifyView.getContext());
                 builder.setTitle("옷장 추가");
 
@@ -50,6 +71,11 @@ public class TabModify {
                         String closetName = input.getText().toString();
                         if (!closetName.isEmpty()) {
                             Toast.makeText(modifyView.getContext(), "입력한 옷장 이름: " + closetName, Toast.LENGTH_SHORT).show();
+                                int dataNo = itemData.size() + 1; // 리스트뷰의 아이템을 차례대로 추가
+                                listView = modifyView.findViewById(R.id.list_item);
+                                listView.setAdapter(tabModifyAdapter);
+                                itemData.add(closetName);
+                                tabModifyAdapter.notifyDataSetChanged();
                         } else {
                             Toast.makeText(modifyView.getContext(), "옷장 이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
                         }
@@ -60,17 +86,20 @@ public class TabModify {
             }
         });
 
-        modify_Modify.setOnClickListener(new View.OnClickListener() {
+        // 수정 - 수정 버튼 동작
+        modify_Modify.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                modify_closet_edit.setVisibility(View.VISIBLE);
-            }
-        });
-
-        modify_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                modify_closet_delete.setVisibility(View.VISIBLE);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                tabModifyAdapter.setVisibility(isChecked);
+                if(modify_Modify.isChecked()) {
+                    modify_Modify.setBackgroundColor(Color.parseColor("#ff6e6e"));
+                    modify_Modify.setText("취소");
+                    modify_Add.setEnabled(false);
+                } else {
+                    modify_Modify.setBackgroundColor(Color.parseColor("#e9ecef"));
+                    modify_Modify.setText("수정");
+                    modify_Add.setEnabled(true);
+                }
             }
         });
     }
